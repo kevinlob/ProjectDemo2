21870beafafb9215e508e0a3d2f087d873a97801
kevinlob <34299699+kevinlob@users.noreply.github.com>
2019-04-02 17:51:09 -0400

    Add files via upload
    Function of game

--- script/index.js
+++ script/index.js
@@ -1,8 +1,60 @@
+
 function doFirst() {
   var button = document.getElementById('button');
   // var button2 = document.getElementById('button2');
   button.addEventListener('click', getNameSingle, false);
   // button2.addEventListener('click', getNameMult, false);
+  var Image1 = document.getElementById('Image1');
+  var Image2 = document.getElementById('Image2');
+  var Image3 = document.getElementById('Image3');
+  var Image4 = document.getElementById('Image4');
+
+  var img = document.getElementById("img1");
+  var Img2 = document.getElementById('img2');
+  var Img3 = document.getElementById('img3');
+  var Img4 = document.getElementById('img4');
+
+  var photo1 = document.getElementById('photo1');
+  photo1.addEventListener('click', zoom, false);
+
+  var photo2 = document.getElementById('photo2');
+  photo2.addEventListener('click', zoom2, false);
+
+  var photo3 = document.getElementById('photo3');
+  photo3.addEventListener('click', zoom3, false);
+
+  var photo4 = document.getElementById('photo4');
+  photo4.addEventListener('click', zoom4, false);
+
+  document.getElementsByClassName("close")[0].onclick = function () {
+    Image1.style.display = "none";
+  }
+  document.getElementsByClassName("close")[1].onclick = function () {
+    Image2.style.display = "none";
+  }
+  document.getElementsByClassName("close")[2].onclick = function () {
+    Image3.style.display = "none";
+  }
+  document.getElementsByClassName("close")[3].onclick = function () {
+    Image4.style.display = "none";
+  }
+
+  document.getElementById('clear').onclick = function () {
+    document.getElementById('guess').value = ""
+  }
+
+  document.getElementById('submit').onclick = function(){
+    if(document.getElementById('guess').value.toLowerCase() == current_Round.answer.toLowerCase()){
+      nextRound();
+      score = score+1
+      document.getElementById('score').innerHTML = "Score: "+ score
+      console.log(current_Round.answer)
+    }
+    else{
+      console.log("INCORRECT");
+      console.log(current_Round.answer)
+    }
+  }
 
 }
 
@@ -21,14 +73,53 @@ function getNameSingle() {
     }
     if (time <= -3) {
       document.getElementById('welcome').style.display = "none";
+      document.getElementById('container').style.display = "block";
+      document.getElementById('logo').style.display = "block";
+      document.getElementById('indexBody').style.animation = "backwards 4s infinite";
     }
   }, 1000), 10000);
 }
 
-  function TriggeredKey(e) {
-    var keycode;
-    if (window.event) keycode = window.event.keyCode;
-    if (window.event.keyCode == 13) return false;
-  }
+function TriggeredKey(e) {
+  var keycode;
+  if (window.event) keycode = window.event.keyCode;
+  if (window.event.keyCode == 13) return false;
+}
 
-  window.addEventListener('load', doFirst, false);
+window.addEventListener('load', doFirst, false);
+
+function zoom() {
+  Image1.style.display = "block";
+  document.getElementById('img1').src = this.src;
+}
+
+function zoom2() {
+  Image2.style.display = "block";
+  document.getElementById('img2').src = this.src;
+}
+
+function zoom3() {
+  Image3.style.display = "block"
+  document.getElementById('img3').src = this.src;
+}
+
+function zoom4() {
+  Image4.style.display = "block"
+  document.getElementById('img4').src = this.src
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+function nextRound(){
+  index = round.indexOf(current_Round)
+  round.splice(index, 1)
+  random = getRandomInt(round.length)
+  current_Round= round[random]
+  document.getElementById('guess').value = "";
+  document.getElementById('photo1').src = current_Round.photo1
+  document.getElementById('photo2').src = current_Round.photo2
+  document.getElementById('photo3').src = current_Round.photo3
+  document.getElementById('photo4').src = current_Round.photo4
+
+}
+var score = 0
---
+++ script/rounds.js
@@ -0,0 +1,147 @@
+const round_1 = {
+  photo1: "img/round 1/image1.jpg", 
+  photo2: "img/round 1/image2.jpg",
+  photo3: "img/round 1/image3.jpg",
+  photo4: "img/round 1/image4.jpg",
+  answer: "Technology"
+};
+const round_2 = {
+  photo1: "img/round 2/image1.jpg", 
+  photo2: "img/round 2/image2.jpg",
+  photo3: "img/round 2/image3.jpg",
+  photo4: "img/round 2/image4.jpg",
+  answer: "Train"
+};
+const round_3 = {
+  photo1: "img/round 3/image1.jpg", 
+  photo2: "img/round 3/image2.jpg",
+  photo3: "img/round 3/image3.jpg",
+  photo4: "img/round 3/image4.jpg",
+  answer: "Heavy"
+};
+const round_4 = {
+  photo1: "img/round 4/image1.jpg", 
+  photo2: "img/round 4/image2.jpg",
+  photo3: "img/round 4/image3.jpg",
+  photo4: "img/round 4/image4.jpg",
+  answer: "Trip"
+};
+const round_5 = {
+  photo1: "img/round 5/image1.jpg", 
+  photo2: "img/round 5/image2.jpg",
+  photo3: "img/round 5/image3.jpg",
+  photo4: "img/round 5/image4.jpg",
+  answer: "Bass"
+};
+const round_6 = {
+  photo1: "img/round 6/image1.jpg", 
+  photo2: "img/round 6/image2.jpg",
+  photo3: "img/round 6/image3.jpg",
+  photo4: "img/round 6/image4.jpg",
+  answer: "Peace"
+};
+const round_7 = {
+  photo1: "img/round 7/image1.jpg", 
+  photo2: "img/round 7/image2.jpg",
+  photo3: "img/round 7/image3.jpg",
+  photo4: "img/round 7/image4.jpg",
+  answer: "swing"
+};
+const round_8 = {
+  photo1: "img/round 8/image1.jpg", 
+  photo2: "img/round 8/image2.jpg",
+  photo3: "img/round 8/image3.jpg",
+  photo4: "img/round 8/image4.jpg",
+  answer: "record"
+};
+const round_9 = {
+  photo1: "img/round 9/image1.jpg", 
+  photo2: "img/round 9/image2.jpg",
+  photo3: "img/round 9/image3.jpg",
+  photo4: "img/round 9/image4.jpg",
+  answer: "Marble"
+};
+const round_10 = {
+  photo1: "img/round 10/image1.jpg", 
+  photo2: "img/round 10/image2.jpg",
+  photo3: "img/round 10/image3.jpg",
+  photo4: "img/round 10/image4.jpg",
+  answer: "slide"
+};
+const round_11 = {
+  photo1: "img/round 11/image1.jpg", 
+  photo2: "img/round 11/image2.jpg",
+  photo3: "img/round 11/image3.jpg",
+  photo4: "img/round 11/image4.jpg",
+  answer: "braces"
+};
+const round_12 = {
+  photo1: "img/round 12/image1.jpg", 
+  photo2: "img/round 12/image2.jpg",
+  photo3: "img/round 12/image3.jpg",
+  photo4: "img/round 12/image4.jpg",
+  answer: "organ"
+};
+const round_13 = {
+  photo1: "img/round 13/image1.jpg", 
+  photo2: "img/round 13/image2.jpg",
+  photo3: "img/round 13/image3.jpg",
+  photo4: "img/round 13/image4.jpg",
+  answer: "fly"
+};
+const round_14 = {
+  photo1: "img/round 14/image1.jpg", 
+  photo2: "img/round 14/image2.jpg",
+  photo3: "img/round 14/image3.jpg",
+  photo4: "img/round 14/image4.jpg",
+  answer: "smash"
+};
+const round_15 = {
+  photo1: "img/round 15/image1.jpg", 
+  photo2: "img/round 15/image2.jpg",
+  photo3: "img/round 15/image3.jpg",
+  photo4: "img/round 15/image4.jpg",
+  answer: "adder"
+};
+
+
+
+const round_16 = {
+  photo1: "img/round 16/image1.jpg", 
+  photo2: "img/round 16/image2.jpg",
+  photo3: "img/round 16/image3.jpg",
+  photo4: "img/round 16/image4.jpg",
+  answer: "wedge"
+};
+const round_17 = {
+  photo1: "img/round 17/image1.jpg", 
+  photo2: "img/round 17/image2.jpg",
+  photo3: "img/round 17/image3.jpg",
+  photo4: "img/round 17/image4.jpg",
+  answer: "fall"
+};
+const round_18 = {
+  photo1: "img/round 18/image1.jpg", 
+  photo2: "img/round 18/image2.jpg",
+  photo3: "img/round 18/image3.jpg",
+  photo4: "img/round 18/image4.jpg",
+  answer: "flake"
+};
+const round_19 = {
+  photo1: "img/round 19/image1.jpg", 
+  photo2: "img/round 19/image2.jpg",
+  photo3: "img/round 19/image3.jpg",
+  photo4: "img/round 19/image4.jpg",
+  answer: "ring"
+};
+const round_20 = {
+  photo1: "img/round 20/image1.jpg", 
+  photo2: "img/round 20/image2.jpg",
+  photo3: "img/round 20/image3.jpg",
+  photo4: "img/round 20/image4.jpg",
+  answer: "skate"
+};
+
+
+var round = [round_1,round_2, round_3, round_4, round_5, round_6, round_7, round_8, round_9, round_10, round_11, round_12, round_13, round_14, round_15, round_16, round_17, round_18, round_19, round_20]
+var current_Round = round_1
